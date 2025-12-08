3e59275aa305148d7d630438596070ca8d47a02a
KengKaK <suppadej3@gmail.com>
2025-12-08 20:23:00 +0700

    Exception error

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,17 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
 
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-client = MongoClient(MONGODB_URL)
+load_dotenv()
 
-db = client["ocr_document_db"]
-users_collection = db["users"]
-documents_collection = db["documents"]
+MONGODB_URL = os.getenv("MONGODB_URL")
+
+try:
+    client = MongoClient(MONGODB_URL)
+    db = client["ocr_document_db"]
+    
+    users_collection = db["users"]
+    documents_collection = db["documents"]
+
+except Exception as e:
+    print("❌ Connection failed:", e)
